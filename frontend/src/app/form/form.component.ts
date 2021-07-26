333c79f034c71bdb0c50ccde6505da7df8104ed1
razvan mustata <razvan.mustata@gmail.com>
2021-07-26 17:01:01 +0300

    added html2pdf

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
 import { FormService } from './form.service';
 import { HttpClient } from '@angular/common/http';
 import { Form } from './form.model';
+// import { jsPDF } from "jspdf";
+// declare let html2canvas: any;
+import jsPDF from 'jspdf';
+import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-form',
@@ -9,6 +17,28 @@ import { Form } from './form.model';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  [x: string]: any;
+  
+  generatePDF() {
+    var data = document.getElementById('pdfTable')!;
+
+    html2canvas(data).then(canvas => {
+      var imgWidth = 200;
+      var imgHeight = 200;
+      // var imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png')
+      var pdf = new jsPDF('p', 'mm', [297, 210]);
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('newPDF.pdf');
+    });
+  }
+
+  title = 'htmltopdf';
+   
+  @ViewChild('pdfTable')
+  pdfTable!: ElementRef;
+  
 
   angajat = {
     // selectedState: '',
@@ -45,7 +75,54 @@ allForms: Array<Form> = [];
   onBasicUploadAuto(angajat:any) {
     console.log(this.angajat);
     this.formService.create_new_form(this.angajat).subscribe((allForms: Form[]) => {
+    this.getAllForms();
     });
   }
+
+download(){
+  var getBase64Image = (document.getElementById("pdfTable"));
+  console.log(getBase64Image);
+  const doc = new jsPDF();
+  // doc.addPage();
+  // doc.setFont("Times New Roman");
+  // doc.setFontSize(10);
+  // doc.text(20, 30, "Budgeted Year:");
+  // doc.setTextColor(0, 0, 0);
+  // doc.text(20, 40, "Actual Occupancy:");
+  // doc.save('FirstPdf.pdf');
+  }
+
+
+htmlToPdf() {
+    let employee: object;
+
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+    // const doc = new jsPDF()
+    var ta = document.getElementById('pdfTable') || '';
+    // console.log(ta);
+    // let final = ta.toString();
+    // const myJSON = JSON.stringify(employee);
+    // console.log(final);
+    // doc.text(myJSON,10, 10);
+    // doc.save('demo.pdf')
+
+    var doc = new jsPDF('l', 'mm', [800, 700]);
+    doc.html(ta, {
+      callback: function (doc) {
+        // doc.setFont("helvetica");
+        // doc.setFontSize(45);
+        doc.save();
+      },
+      x: 1,
+      y: 1
+    });
+}
+
 
 }
